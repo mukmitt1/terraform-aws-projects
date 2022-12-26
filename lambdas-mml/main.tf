3be9470e944e429398304f3f3ab1d534aafaa287
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-26 13:19:21 -0500

    add policy

--- lambdas-mml/main.tf
+++ lambdas-mml/main.tf
@@ -21,3 +21,26 @@ EOF
 provider "aws" {
   region = var.aws_region
 }
+
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
