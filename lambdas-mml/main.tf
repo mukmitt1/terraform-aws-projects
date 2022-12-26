66c39391d81bedc618e07c16fefd0d282fc4ef1a
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-26 13:33:05 -0500

    update

--- lambdas-mml/main.tf
+++ lambdas-mml/main.tf
@@ -50,4 +50,3 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
  role        = aws_iam_role.lambda_role.name
  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
 }
-2. 
